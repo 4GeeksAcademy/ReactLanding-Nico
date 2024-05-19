bed9366572a4c8b19d0f42053c7a4363e3ccb4c0
Alvaro Nicolas Ortiz <160544793+ANicolasOrtiz@users.noreply.github.com>
2024-05-19 20:41:50 +0000

    componentes casi terminados

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -3,15 +3,18 @@ import React from "react";
 export const Card= (props) => {
 
     return(
-    <div className="card" style={{"width": 20 + "rem"}}>
-    <img src={props.imgSrc} className="card-img-top" alt="..."/>
-    <div className="card-body">
-      <h5 className="card-title">{props.title}</h5>
-      <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores exercitationem, doloribus laboriosam iste
-      optio et cum eum dolor, obcaecati facilis tenetur? Quaerat, aperiam esse temporibus pariatur vel deleniti veritatis necessitatibus.</p>
-      <a href="#" className="btn btn-primary">Find out more!</a>
+    <div className="card d-flex mx-auto" style={{"width": 20 + "rem"}}>
+      <img src={props.imgSrc} className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">{props.title}</h5>
+        <p className="card-text text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores exercitationem, doloribus laboriosam iste
+        optio et cum eum dolor, obcaecati facilis tenetur? Quaerat, aperiam esse temporibus pariatur vel deleniti veritatis necessitatibus.</p>
+        <div className="text-center">
+          <a href="#" className="btn btn-primary">Find out more!</a>
+        </div>
+      </div>
     </div>
-  </div>
+  
   );
 }
 
--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -12,7 +12,7 @@ export const Navbar = () => {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
+            <div className="navbar-nav ms-auto">
               <a className="nav-link active" aria-current="page" href="#">Home</a>
               <a className="nav-link" href="#">About</a>
               <a className="nav-link" href="#">Services</a>
