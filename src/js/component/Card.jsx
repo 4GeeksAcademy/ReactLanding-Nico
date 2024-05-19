b07f7a9f3add946031042ee7ec6cec491bdf5dcb
Alvaro Nicolas Ortiz <160544793+ANicolasOrtiz@users.noreply.github.com>
2024-05-19 22:27:33 +0000

    componentes terminados

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export const Card= (props) => {
 
     return(
-    <div className="card d-flex mx-auto" style={{"width": 20 + "rem"}}>
-      <img src={props.imgSrc} className="card-img-top" alt="..."/>
+    <div className="card" style={{ width: '22rem', margin: '20px'}}>
+      <img src={props.imgSrc} className="card-img-top"  alt="..."/>
       <div className="card-body">
-        <h5 className="card-title">{props.title}</h5>
+        <h5 className="card-title text-center">{props.title}</h5>
         <p className="card-text text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores exercitationem, doloribus laboriosam iste
         optio et cum eum dolor, obcaecati facilis tenetur? Quaerat, aperiam esse temporibus pariatur vel deleniti veritatis necessitatibus.</p>
         <div className="text-center">
@@ -14,7 +14,6 @@ export const Card= (props) => {
         </div>
       </div>
     </div>
-  
   );
 }
 
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -11,12 +11,16 @@ const Home = () => {
 		<div>
 			<Navbar/>
 			<Jumbotron/>
+		<div className="d-flex">
+		
 			<Card title="Dog" imgSrc="https://www.elmueble.com/medio/2023/05/03/perro-de-raza-podenco_39e28ed8_230503130255_900x900.jpg"/>
 			<Card title="Food" imgSrc="https://www.simplyrecipes.com/thmb/0pVK4YpDJNoXPEAchI_OSDE0UZQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Chicago-Deep-Dish-Pizza-LEAD-5-f8c04d3d77b24e479bbb1502758a5ed1.jpg"/>
 			<Card title="Nature" imgSrc="https://www.camepe.com/wp-content/uploads/2021/04/quilotoa-3.jpg"/>
 			<Card title="Soccer" imgSrc="https://pbs.twimg.com/profile_images/1663361443587149824/gziPB8mg_400x400.jpg"/>
 		</div>
+		</div>
 	);
 };
 
 export default Home;
+
